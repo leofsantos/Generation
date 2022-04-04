e0ed502c50d58c85db543e093e17d32910944262
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-04-04 14:27:09 -0300

    Adicionando Exercício 5 da Lista 3

---
+++ Java/src/Turma51/Lista3/Exercicio5.java
@@ -0,0 +1,23 @@
+package Turma51.Lista3;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+	public static void main(String[] agrs) {
+		
+		int num, soma=0;
+		
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite um número do seu teclado: ");
+		num = ler.nextInt();
+		
+		do
+		{
+			System.out.print("Digite um número do seu teclado: ");
+			num = ler.nextInt();
+			soma+=num;
+		}
+		while(num!=0);
+		System.out.println("\nA soma dos números digitados vale: "+soma);
+	}
+}
