d9a62fd9ce0673131570d0a5000d95f874e2f092
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-04-05 19:27:23 -0300

    Adicionando Exercício 2 da Lista 4

---
+++ Java/src/Turma51/Lista4/Exercicio2.java
@@ -0,0 +1,46 @@
+package Turma51.Lista4;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+	public static void main(String[] args) {
+		
+		int numero[] = new int[6];
+		int n,x,somaPar=0,contImpar=0;
+		Scanner leia = new Scanner(System.in);
+		
+		for(x=0;x<6;x++)
+		{
+			System.out.printf("Entre com o %dº número: ",x+1);
+			n = leia.nextInt();
+			numero[x] = n;
+			if (n%2==0)
+			{
+				somaPar+=numero[x];
+			}
+			else
+			{
+				contImpar++;
+			}
+		}
+		System.out.print("\n\n* * * * * * * * * * * * * * * * * * *\n\n");
+		System.out.print("\nOs pares digitados foram: ");
+		for(x=0;x<numero.length;x++)
+		{
+			if(numero[x]%2==0)
+			{
+				System.out.print("["+numero[x]+"] ");
+			}
+		}
+		System.out.print("\nA soma dos pares vale: "+somaPar);
+		System.out.print("\nOs ímpares digitados foram: ");
+		for(x=0;x<numero.length;x++)
+		{
+			if(numero[x]%2!=0)
+			{
+				System.out.print("["+numero[x]+"] ");
+			}
+		}
+		System.out.print("\nTotalizando: "+contImpar+" ímpares.");
+	}
+}
