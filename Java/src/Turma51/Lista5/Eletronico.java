4e90cf95f46b80fdcb6d9c7585340fa3f76462db
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-04-07 20:48:21 -0300

    Adicionando novos métodos ao Exercício 3 da Lista 5

--- Java/src/Turma51/Lista5/Eletronico.java
+++ Java/src/Turma51/Lista5/Eletronico.java
@@ -6,16 +6,18 @@ public class Eletronico {
 		private String modelo;
 		private int ano;
 		private double custo;
+		private double precoFinal = 0;
 		
 		//constructor method
 		
-		public Eletronico(String tipo, String modelo, int ano, double custo) 
+		public Eletronico(String tipo, String modelo, int ano, double custo,double precoFinal) 
 		{
 			super();
 			this.tipo = tipo;
 			this.modelo = modelo;
 			this.ano = ano;
 			this.custo = custo;
+			this.precoFinal = precoFinal;
 		}
 		
 		//getters and setters
@@ -51,8 +53,44 @@ public class Eletronico {
 		public void setCusto(float custo) {
 			this.custo = custo;
 		}
+		
+		public double getPrecoFinal() {
+			return precoFinal;
+		}
+
+		public void setPrecoFinal(double precoFinal) {
+			this.precoFinal = precoFinal;
+		}
+
 		public void fichaTecnica()
 		{
+			System.out.print("\t\t FICHA TÉCNICA \n\n");
 			System.out.print("\nTipo: "+tipo+"\nModelo: "+modelo+"\nAno: "+ano+"\nCusto de Fábrica: R$ "+custo);
+		}
+		public void venda(double comissao)
+		{
+			double precoVenda = this.custo+comissao;
+			this.precoFinal = precoVenda;
+			System.out.print("\n- - - - - - - - - - - - - - - - - - - - - - - -");
+			System.out.println("\nO valor de venda desse produto é de: R$"+precoVenda);
+			System.out.print("\n\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n\n");
+		}
+		public void estado()
+		{
+			if(this.ano>=2022)
+			{
+				System.out.print("\n- - - - - - - - - - - - - - - - - - - - - - - -");
+				System.out.print("\nO "+getModelo()+" foi lançado neste ano.");
+			}
+			else if(this.ano<2022 && this.ano>2015)
+			{
+				System.out.print("\n- - - - - - - - - - - - - - - - - - - - - - - -");
+				System.out.print("\nO "+getModelo()+" foi lançado há menos de 10 anos.");
+			}
+			else if(this.ano<2015)
+			{
+				System.out.print("\n- - - - - - - - - - - - - - - - - - - - - - - -");
+				System.out.print("\nO "+getModelo()+" foi lançado há mais de 10 anos.");
+			}
 		}
 }
--- Java/src/Turma51/Lista5/TesteEletronico.java
+++ Java/src/Turma51/Lista5/TesteEletronico.java
@@ -3,16 +3,20 @@ package Turma51.Lista5;
 public class TesteEletronico {
 	public static void main(String args[]) {
 		
-		Eletronico eletronico1 = new Eletronico("Celular","iPhone 12",2020,5000);
-		Eletronico eletronico2 = new Eletronico("Celular","Nokia 3310",2000,300);
-		Eletronico eletronico3 = new Eletronico("Notebook","Dell Inspiron 15 3000",2019,2500);
+		Eletronico eletronico1 = new Eletronico("Celular","iPhone 12",2020,5000,0);
+		Eletronico eletronico2 = new Eletronico("Celular","Nokia 3310",2000,300,0);
+		Eletronico eletronico3 = new Eletronico("Notebook","Dell Inspiron 15 3000",2019,2500,0);
 		
 		eletronico1.fichaTecnica();
-		System.out.print("\n\n* * * * * * * * * * * * * * * \n");
-		eletronico2.fichaTecnica();
-		System.out.print("\n\n* * * * * * * * * * * * * * * \n");
-		eletronico3.fichaTecnica();
-		System.out.print("\n\n* * * * * * * * * * * * * * * \n\n");
+		eletronico1.estado();
+		eletronico1.venda(1500);
 		
+		eletronico2.fichaTecnica();
+		eletronico2.estado();
+		eletronico2.venda(200);
+		
+		eletronico3.fichaTecnica();
+		eletronico3.estado();
+		eletronico3.venda(3000);
 	}
 }
