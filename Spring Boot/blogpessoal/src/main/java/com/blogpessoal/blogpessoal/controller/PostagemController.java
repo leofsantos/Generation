89c8f4fe6f9d90b738156b25455dbcf761752345
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-04-29 15:24:26 -0300

    Adicionando Método Delete ao Controller

--- Spring Boot/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
+++ Spring Boot/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
@@ -1,6 +1,7 @@
 package com.blogpessoal.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,13 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.blogpessoal.blogpessoal.model.Postagem;
 import com.blogpessoal.blogpessoal.repository.PostagemRepository;
@@ -55,4 +59,16 @@ public class PostagemController {
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem)))
 				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = repository.findById(id);
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		repository.deleteById(id);
+	}	
+	
+	
 }
