291fe4994d4b176912d7b24816f76ea80fd4c14c
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-04-29 11:25:20 -0300

    Adicionando ao controller método de retornar postagem a partir de um id específico

--- Spring Boot/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
+++ Spring Boot/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
@@ -3,6 +3,7 @@ package com.blogpessoal.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +23,17 @@ public class PostagemController {
 	private PostagemRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<Postagem>> GetAll(){
+	public ResponseEntity<List<Postagem>> GetAllPostagem(){
 		return ResponseEntity.ok(repository.findAll());
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return repository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
